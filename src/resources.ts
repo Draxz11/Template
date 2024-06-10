a450ae7a5c55f89690c06a6ed12a03671ce8386a
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-06-10 17:23:31 -0300

    feat: Mapa adicionado

--- src/resources.ts
+++ src/resources.ts
@@ -3,11 +3,31 @@ import sword from "./images/sword.png";
 import logo from "./images/logo.png"
 import imagem from "./images/logo-vertical.png"
 
+import pngTilesetPath from "./Maps/Room_Builder_32x32.png?url"
+
+import tsxParedesPath from "./Maps/tileset_paredes.tsx?url"
+import tsxGenericPath from "./Maps/tileset_generic.tsx?url"
+import tsxEstoquePath from "./Maps/tileset_estoque.tsx?url"
+import tsxBibliotecaPath from "./Maps/tileset_biblioteca.tsx?url"
+
+import tmxMapaPath from "./Maps/Showroom_map.tmx?url"
+import { TiledResource } from "@excaliburjs/plugin-tiled";
+
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
   LogoVertical: new ImageSource(imagem),
-  Gamificacapo: new ImageSource(imagem)
+  Gamificacapo: new ImageSource(imagem),
+  Mapa: new TiledResource(tmxMapaPath, {
+    pathMap: [
+      { path: "showroom_map.tmx", output: tmxMapaPath },
+      { path: "Room_Builder_32x32.png", output: pngTilesetPath },
+      { path: "tileset_paredes.tsx", output: tsxParedesPath },
+      { path: "tileset_generic.tsx", output: tsxGenericPath },
+      { path: "tileset_estoque.tsx", output: tsxEstoquePath },
+      { path: "tileset_biblioteca.tsx", output: tsxBibliotecaPath }
+    ]
+  })
 } as const;
 
 export const loader = new Loader();
--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,5 @@
-import { Color, FadeInOut, Scene, Transition } from "excalibur";
+import { Color, Engine, FadeInOut, Resource, Scene, Transition } from "excalibur";
+import { Resources } from "../resources";
 
 export class expoScene extends Scene{
     onTransition(direction: "in" | "out"): Transition | undefined {
@@ -9,5 +10,12 @@ export class expoScene extends Scene{
         })
     }
 
+    onInitialize(engine: Engine<any>): void {
+        // Carregar o mapa
+        let tiledMap = Resources.Mapa
+
+        // Adicionar o mapa na cena
+        tiledMap.addToScene(this)
+    }
     
 }
