ee59fd91855aec7dc523fd8d21367e2ea6996b45
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-06-11 17:24:34 -0300

    feat: Plaer adicionado

---
+++ src/actors/player.ts
@@ -0,0 +1,91 @@
+import { Actor, Color, Engine, Keys, vec } from "excalibur";
+
+export class Player extends Actor {
+    // Propiedade do player
+    private velocidade: number = 180
+
+
+    // Configuração do Player
+    constructor() {
+        super({
+            pos: vec(600, 500),
+            width: 32,
+            height: 32,
+            name: "Jugador",
+            color: Color.Red
+        })
+    }
+
+    onInitialize(engine: Engine<any>): void {
+        // Configurar player para monitorar evento "hold" -> segurar tecla
+        engine.input.keyboard.on("hold", (event) => {
+            // Detectar qual tecla está pressionada
+            switch(event.key) {
+                case Keys.Left:
+                case Keys.A:
+                // Gambiarra para colocar para a esquerda -> case Keys.A
+                // Mover para a esquerda
+                // Define a velocidade x para negativa, que significa movimentar o player para a esquerda
+                this.vel.x = -this.velocidade
+                break;
+
+                case Keys.Right:
+                case Keys.D:
+                    // Mover para a direita
+                    // Define a velocidade x para negativa, que significa movimentar o player para a direita
+                    this.vel.x = this.velocidade
+                    break;
+
+                case Keys.Up:
+                case Keys.W:
+                    // Mover para cima
+                    // Define a velocidade y para negativa, que significa movimentar o player para cima
+                    this.vel.y = -this.velocidade
+                    break;
+
+                case Keys.Down:
+                case Keys.S:
+                    // Mover para baixo
+                    // Define a velocidade y para negativa, que significa movimentar o player para baixo
+                    this.vel.y = this.velocidade
+                     break;
+
+                default:
+                    // Zera a velocidade do Player, PARA a movimentação
+                    this.vel.x = 0
+                    this.vel.y = 0
+
+                    break;
+
+                    // this.vel = vec(0, 0)
+            }
+        })
+
+        // Configura o player para monitorar o evento "release" -> soltar
+        engine.input.keyboard.on("release", (event) => {
+            // Fazer o player parar ao soltar as teclas de movimentação
+            // Parar movimentação lateral ao soltar as teclas dew movimentação lateral
+            if(
+                event.key == Keys.A ||
+                event.key == Keys.Left ||
+                event.key == Keys.D ||
+                event.key == Keys.Right 
+            ) {
+                //Zerar velocidade horizontal
+                this.vel.x = 0
+            }
+
+            // Para movimentação vertical ao soltar as teclas de movimentação vertical
+            if(
+                event.key == Keys.W ||
+                event.key == Keys.Up ||
+                event.key == Keys.S ||
+                event.key == Keys.Down
+            ) {
+                // Zerar velocidade vertical
+                this.vel.y = 0
+            }
+        })
+    }
+
+}
