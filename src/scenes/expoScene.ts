429c0c6ec468c4a638882e984b07a92542761ae5
MIGUEL SINHORETI CHIOVATTO <42255054809@info.local>
2024-06-12 17:18:17 -0300

    feat:colisão certa com a parede

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,4 @@
-import { Color, Engine, FadeInOut, Resource, Scene, Transition, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, FadeInOut, Resource, Scene, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
 import { Player } from "../actors/player";
 
@@ -36,6 +36,30 @@ export class expoScene extends Scene{
 
         // Adicionar o Player na cena
         this.add(jogador)
+
+        // Adicionar clisão com cada objeto
+        // Pegar a camada de objetos colisores
+        let camadaObjetoColisores = tiledMap.getObjectLayers("ObjetosColisores")[0]
+
+        console.log(camadaObjetoColisores);
+
+        // Percorrer abjetos com foreach e para cada objeto,renderizar um actor
+        camadaObjetoColisores.objects.forEach(objeto => {
+            // Configurar actor
+            const objetoAtual = new Actor({
+                name: objeto.name,
+                x: objeto.x + offsetX + (objeto.tiledObject.width! / 2),
+                y: objeto.y + offsetY + (objeto.tiledObject.height! / 2),
+                width:objeto.tiledObject.height,
+                collisionType: CollisionType.Fixed,
+                color: Color.Blue,
+                z: 99
+            })
+
+            // Adicionar o colisor do objeto na cena
+            this.add(objetoAtual)
+        })
+
     }
     
 }
